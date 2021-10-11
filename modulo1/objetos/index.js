20b6667c8e6ce7772d0e786f1afd4f70b7598f87
SORAIAROCHA <soraiarsr2017@gmail.com>
2021-10-11 22:28:49 +0100

    objetos

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,48 @@
+/*
+1
+a:
+Matheus Nachtergaele
+Virginia Cavendish
+canal:"Globo",
+horario:"14h"
+
+2
+a:
+nome:"Juca",
+idade:3,
+raca:"SRD"
+
+nome:"Juba",
+idade:3,
+raca:"SRD"
+
+nome:"Jubo",
+idade:3,
+raca:"SRD"
+
+b:A sintaxe dos três pontos antes do nome de um objeto copia um objeto ou um array, 
+podendo a cópia ser modificada sem afetar o original.
+
+3
+a:
+false
+undefined
+
+b:Dentro do escopo da função minhaFuncao tem o objeto pessoa com a propriedade
+ backender, que por sua vez é um boolean.
+ Já altura não se encontra dentro do objeto pessoa.
+ */
+
+ 
+   //1
+   
+   const pessoa = {
+        nome: "Soraia", 
+        apelidos: ["Soso", "Sol", "Sô"]
+    }
+    function nomeApelidos(pessoa)
+        const frase: 'Eu sou ${nome} , mas pode me chamar de ${apelidos}'
+        return frase
+        console.log (pessoa.frase)
+
+ 
