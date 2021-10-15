d38cb075aa9fcb99fe78e5a703d390b285760f86
SORAIAROCHA <soraiarsr2017@gmail.com>
2021-10-15 21:51:37 +0100

    funcoes-de-array

---
+++ modulo1/funcoes-de-array/index.js
@@ -0,0 +1,42 @@
+
+/*
+1
+{
+nome:"Amanda Rangel",
+apelido:"Mandi"
+}
+0
+(3) [
+{...},
+{...},
+{...}]
+{
+nome:"Laís Petra",
+apelido:"Laura"
+}
+1
+(3) [
+{...},
+{...},
+{...}]
+{
+nome:"Letícia Chijo",
+apelido:"Chijo"
+}
+2
+(3) [
+{...},
+{...},
+{...}]
+
+2
+(3) [
+"Amanda Rangel",
+"Laís Petra",
+"Letícia Chijo"
+]
+
+3 
+(2) [
+{...},
+{...}]
